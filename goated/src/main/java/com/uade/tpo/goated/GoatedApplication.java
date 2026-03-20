4b35775011ceadc61d2666a9efe8ad8ec84867c8
LucasC <castrolucas98@gmail.com>
2026-03-20 19:04:46 -0300

    Primer commit

---
+++ goated/src/main/java/com/uade/tpo/goated/GoatedApplication.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.goated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoatedApplication.class, args);
+	}
+
+}
---
+++ goated/src/test/java/com/uade/tpo/goated/GoatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.goated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
