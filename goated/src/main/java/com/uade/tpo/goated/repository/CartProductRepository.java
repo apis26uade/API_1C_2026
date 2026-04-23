6aa7dc281dd86f32c35b8e40f9080fd217c4ae7f
valecue <valentiinacuello@gmail.com>
2026-04-23 16:42:45 -0300

    fix: agregar @Modifying y @Transactional a deleteByCartIdCart

--- goated/src/main/java/com/uade/tpo/goated/repository/CartProductRepository.java
+++ goated/src/main/java/com/uade/tpo/goated/repository/CartProductRepository.java
@@ -3,10 +3,15 @@ package com.uade.tpo.goated.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.uade.tpo.goated.entity.CartProduct;
 
 public interface CartProductRepository extends JpaRepository<CartProduct, Long> {
     List<CartProduct> findByCartIdCart(Long cartId);
+
+    @Modifying
+    @Transactional
     void deleteByCartIdCart(Long cartId);
 }
